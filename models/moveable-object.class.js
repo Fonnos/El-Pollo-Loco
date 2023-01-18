f5e2bb29703f01a31a2bae1f5f1c8fef932be377
Fonnos <eric.panhans@gmail.com>
2023-01-18 22:21:21 +0100

    add first images

--- models/moveable-object.class.js
+++ models/moveable-object.class.js
@@ -4,6 +4,12 @@ class MoveableObject {
     img;
 
 
+    loadImage(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+
     moveRight() {
         console.log('Moving right')
     }
