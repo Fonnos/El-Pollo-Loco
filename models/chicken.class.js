2851ab42654303c61309af585e51f64a3249fc9f
Fonnos <eric.panhans@gmail.com>
2023-02-09 21:01:52 +0100

    walking animation chicken

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -2,13 +2,31 @@ class Chicken extends MoveableObject {
     height = 100;
     width = this.height;
     y = 440 - this.height;  //flexibele y durch unterschiedliche höhe der chicken
+    IMAGES_WALKING = [
+        'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
+        'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
+    ];
     
 
 
     constructor() {
-        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png')
+        super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
+        this.loadImages(this.IMAGES_WALKING);
+        this.x = 200 + Math.random() * 500; // Zahl zwischen 200 und 700;
+        this.animate();
 
-        this.x = 200 + Math.random() * 500; // Zahl zwischen 200 und 700
+        
+    }
+
+
+    animate() {
+        setInterval(() => {
+            let i = this.currentImage % this.IMAGES_WALKING.length; //ermitteln des restes 
+            let path = this.IMAGES_WALKING[i];
+            this.img = this.imageCache[path];
+            this.currentImage++;
+        }, 100);  
     }
 
 
