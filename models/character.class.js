2d12498eedc4ef0286bfb216cb5d9a62fa8d6bed
Fonnos <eric.panhans@gmail.com>
2023-02-09 23:14:26 +0100

    key event listener

--- models/character.class.js
+++ models/character.class.js
@@ -10,14 +10,18 @@ class Character extends MoveableObject {
         'img/2_character_pepe/2_walk/W-25.png',
         'img/2_character_pepe/2_walk/W-26.png'
     ];
+
+    world;
     
     
 
     constructor() {
+        
         super().loadImage('img/2_character_pepe/1_idle/idle/I-1.png');
         this.loadImages(this.IMAGES_WALKING);
 
         this.animate();
+        
         
     }    
            
@@ -25,13 +29,23 @@ class Character extends MoveableObject {
 
 
     animate() {
-        setInterval(() => {
-            let i = this.currentImage % this.IMAGES_WALKING.length; //ermitteln des restes 
-            let path = this.IMAGES_WALKING[i];
-            this.img = this.imageCache[path];
-            this.currentImage++;
-        }, 100);  
-    }
+        console.log(this.world);
+            setInterval(() => {
+                if (this.world.keyboard.RIGHT) {
+                    let i = this.currentImage % this.IMAGES_WALKING.length; //ermitteln des restes 
+                let path = this.IMAGES_WALKING[i];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+                }
+                
+            }, 100); 
+        }
+
+            
+       
+
+         
+    
     
 
 
